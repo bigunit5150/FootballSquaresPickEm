9bffcb1f3d5596356a440c1f1bf5ece2afb7e27b
keithsmith5150 <keith.smith@boostmedia.com>
2015-05-24 19:47:18 -0700

    Installed Identity Framework

---
+++ FSPE.API/DAL/Models/ApplicationUser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace FSPE.API.DAL.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        [MaxLength(100)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string LastName { get; set; }
+
+        [Required]
+        public DateTime JoinDate { get; set; }
+    }
+}
